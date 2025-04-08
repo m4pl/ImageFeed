1a951ad1fde15bb80dfff9dd2d3fd0e3759a28cc
mpplokhov <mpplokhov@MSK-YLFD4CX314.local>
2025-04-08 16:45:00 +0300

    Add profile layout

---
+++ ImageFeed/Profile/Presenter/ProfileViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  ProfileViewModel.swift
+//  ImageFeed
+//
+//  Created by mpplokhov on 08.04.2025.
+//
+
+import UIKit
+
+struct ProfileViewModel {
+    let avatarImage: UIImage
+    let name: String
+    let loginName: String
+    let description: String
+}
---
+++ ImageFeed/Profile/View/ProfileViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by mpplokhov on 08.04.2025.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    @IBOutlet private var avatarImageView: UIImageView!
+    @IBOutlet private var nameLabel: UILabel!
+    @IBOutlet private var loginNameLabel: UILabel!
+    @IBOutlet private var descriptionLabel: UILabel!
+    @IBOutlet private var logoutButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+
+    @IBAction private func logoutButtonTapped(_ sender: UIButton) {
+        print("Logout tapped")
+    }
+
+    private func setupUI() {
+        avatarImageView.image = UIImage(named: "avatar")
+        nameLabel.text = "Екатерина Новикова"
+        loginNameLabel.text = "@ekaterina_nov"
+        descriptionLabel.text = "Hello, World!"
+    }
+}
